7f66b8a020c2f397bc598fdc0ab757c081935277
Dima Khomenko <dkhomenko@mementia.com>
2018-01-24 20:39:47 +0200

    init commit

---
+++ Rce/Calculator/Test/Api/CalculatorTest.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Copyright 2015 Magento. All rights reserved.
+ */
+
+namespace Rce\Calculator\Test\Api;
+
+use Magento\Framework\Webapi\Rest\Request;
+use Magento\InventoryApi\Api\Data\StockInterface;
+use Magento\TestFramework\Assert\AssertArrayContains;
+use Magento\TestFramework\TestCase\WebapiAbstract;
+
+
+/**
+ * Defines the implementation class of the calculator service contract.
+ */
+class CalculatorTest extends WebapiAbstract
+{
+
+    public function testCalculate()
+    {
+        $serviceInfo = [
+            'rest' => [
+                'resourcePath' => '/V1/rce/calculator',
+                'httpMethod' => \Magento\Framework\Webapi\Rest\Request::HTTP_METHOD_POST,
+            ],
+        ];
+
+        $requestData = [
+            'left' => 1.42,
+            'right' => 2.4567,
+            'operator' => 'add',
+            'precision' => 2
+        ];
+
+        $response = $this->_webApiCall($serviceInfo, $requestData);
+
+        $this->assertEquals('Ok.', $response['status']);
+        $this->assertEquals(3.88, $response['result']);
+    }
+
+}
